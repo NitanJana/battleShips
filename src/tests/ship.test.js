a2995e018a6f114f160cf2d200f66f380c92c931
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-05 20:36:46 +0530

    test: add ship object tests

---
+++ src/tests/ship.test.js
@@ -0,0 +1,24 @@
+import Ship from "../modules/ship";
+
+let newShip;
+beforeEach(() => {
+  newShip = Ship(3);
+});
+
+test("Ship factory should return a valid ship object", () => {
+  expect(newShip.getLength()).toBe(3);
+  expect(newShip.getHits()).toBe(0);
+  expect(newShip.isSunk()).toBe(false);
+});
+
+test("Hit count should increase when hit method is called", () => {
+  newShip.hit();
+  expect(newShip.getHits()).toBe(1);
+});
+
+test("isSunk should return true when all ship segments are hit", () => {
+  newShip.hit();
+  newShip.hit();
+  newShip.hit();
+  expect(newShip.isSunk()).toBe(true);
+});
