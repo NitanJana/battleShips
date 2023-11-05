f03e71f7e97b2b491ab8dc303ffcec6826d62968
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-05 20:39:02 +0530

    feat: add ship factory

---
+++ src/modules/ship.js
@@ -0,0 +1,18 @@
+const Ship = (length) => {
+  let hits = 0;
+
+  const getLength = () => length;
+  const getHits = () => hits;
+  const isSunk = () => hits >= length;
+  const hit = () => {
+    hits += 1;
+  };
+  return {
+    getLength,
+    getHits,
+    isSunk,
+    hit,
+  };
+};
+
+export default Ship;
