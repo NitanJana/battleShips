08b850bd44ae5abb497ef3e84682649a0ef2aa10
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-08 21:06:48 +0530

    feat: add player attack method

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,8 +1,25 @@
 const Player = (name) => {
+  const hitRecord = new Set();
+  const hasAlreayHit = (row, column) => hitRecord.has(`${row}-${column}`);
+  const isInvalidPosition = (row, column, size) =>
+    row < 0 || column < 0 || row >= size || column >= size;
+
   const getName = () => name;
+
+  const attack = (row, column, gameBoard) => {
+    if (hasAlreayHit(row, column)) return false;
+    if (!isInvalidPosition(row, column, gameBoard.getBoard().length)) {
+      gameBoard.recieveAttack(row, column);
+      hitRecord.add(`${row}-${column}`);
+      console.log(hitRecord);
+      return true;
+    }
+    return false;
+  };
 
   return {
     getName,
+    attack,
   };
 };
 
