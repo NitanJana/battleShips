7f36fd28417a9d04ce2e51d1437f32b50d9c60f9
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-09 09:39:31 +0530

    refactor: use beforeEach

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,20 +1,23 @@
 import Player from "../modules/player";
 import GameBoard from "../modules/gameBoard";
 
+let player1;
+let gameBoard;
+
+beforeEach(() => {
+  player1 = Player("player1");
+  gameBoard = GameBoard();
+});
+
 test("player factory returns a valid Player object", () => {
-  expect(Player("player1").getName()).toBe("player1");
+  expect(player1.getName()).toBe("player1");
 });
 
 test("player cannot attack same cell twice", () => {
-  const player1 = Player("player1");
-  const gameBoard = GameBoard();
   expect(player1.attack(4, 2, gameBoard)).toBe(true);
   expect(player1.attack(4, 2, gameBoard)).toBe(false);
 });
 
 test("player cannot attack invalid cell", () => {
-  const player1 = Player("player1");
-  const gameBoard = GameBoard();
   expect(player1.attack(11, 2, gameBoard)).toBe(false);
-  
 });
