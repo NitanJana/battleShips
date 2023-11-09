de63db61ce46f180fac2fcd83507b0cbb66ba193
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-09 09:52:55 +0530

    feat: add player random attack method

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,8 +1,13 @@
 const Player = (name) => {
   const hitRecord = new Set();
+
   const hasAlreayHit = (row, column) => hitRecord.has(`${row}-${column}`);
+
   const isInvalidPosition = (row, column, size) =>
     row < 0 || column < 0 || row >= size || column >= size;
+
+  const getRandomPosition = (gameBoard) =>
+    Math.floor(Math.random() * gameBoard.getBoard().length);
 
   const getName = () => name;
 
@@ -11,15 +16,29 @@ const Player = (name) => {
     if (!isInvalidPosition(row, column, gameBoard.getBoard().length)) {
       gameBoard.recieveAttack(row, column);
       hitRecord.add(`${row}-${column}`);
-      console.log(hitRecord);
       return true;
     }
     return false;
   };
 
+  const randomAttack = (gameBoard) => {
+    if (hitRecord.size === 100) return false;
+    let randomRow = getRandomPosition(gameBoard);
+    let randomCol = getRandomPosition(gameBoard);
+
+    while (hasAlreayHit(randomRow, randomCol)) {
+      randomRow = getRandomPosition(gameBoard);
+      randomCol = getRandomPosition(gameBoard);
+    }
+    gameBoard.recieveAttack(randomRow, randomCol);
+    hitRecord.add(`${randomRow}-${randomCol}`);
+    return true;
+  };
+
   return {
     getName,
     attack,
+    randomAttack,
   };
 };
 
