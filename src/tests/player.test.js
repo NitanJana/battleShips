64127858054f6e5d2834f787dfb6a338b0ab1e4e
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-09 09:52:35 +0530

    test: add player random attack test

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,23 +1,35 @@
 import Player from "../modules/player";
 import GameBoard from "../modules/gameBoard";
 
-let player1;
+let player;
 let gameBoard;
 
 beforeEach(() => {
-  player1 = Player("player1");
+  player = Player("player");
   gameBoard = GameBoard();
 });
 
 test("player factory returns a valid Player object", () => {
-  expect(player1.getName()).toBe("player1");
+  expect(player.getName()).toBe("player");
 });
 
 test("player cannot attack same cell twice", () => {
-  expect(player1.attack(4, 2, gameBoard)).toBe(true);
-  expect(player1.attack(4, 2, gameBoard)).toBe(false);
+  expect(player.attack(4, 2, gameBoard)).toBe(true);
+  expect(player.attack(4, 2, gameBoard)).toBe(false);
 });
 
 test("player cannot attack invalid cell", () => {
-  expect(player1.attack(11, 2, gameBoard)).toBe(false);
+  expect(player.attack(11, 2, gameBoard)).toBe(false);
+});
+
+test("should make a random attack on the game board", () => {
+  expect(player.randomAttack(gameBoard)).toBe(true);
+});
+
+test("should not make an attack if all positions have been hit", () => {
+  // Fill the hitRecord to simulate all positions being hit
+  for (let i = 0; i < 100; i += 1) {
+    player.randomAttack(gameBoard);
+  }
+  expect(player.randomAttack(gameBoard)).toBe(false);
 });
