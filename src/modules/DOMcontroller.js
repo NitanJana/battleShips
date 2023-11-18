f1b8089ec84c885770270481b99f05e8d6d15764
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-18 15:57:03 +0530

    refactor: add proper comments

--- src/modules/DOMcontroller.js
+++ src/modules/DOMcontroller.js
@@ -1,7 +1,8 @@
+// Import the Ship class from the ship module
 import Ship from "./ship";
 
 const DOMcontroller = () => {
-  // Function to create a single cell
+  // Function to create a single cell element
   const createCell = (rowIndex, columnIndex) => {
     const cell = document.createElement("div");
     cell.classList.add("cell");
@@ -10,7 +11,7 @@ const DOMcontroller = () => {
     return cell;
   };
 
-  // Function to create a single row
+  // Function to create a single row element
   const createRow = (rowArray, rowIndex) => {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -21,7 +22,7 @@ const DOMcontroller = () => {
     return row;
   };
 
-  // Function to render the game board
+  // Function to render the game board on the HTML page
   const renderGameBoard = (gameBoard) => {
     const boardArray = gameBoard.getBoard();
     const board = document.createElement("div");
@@ -34,16 +35,16 @@ const DOMcontroller = () => {
     return board;
   };
 
+  // Function to render the ships on the game board
   const renderShips = (gameBoard) => {
     const boardArray = gameBoard.getBoard();
-    // const boardContainer =
-    //   player.getName() === "user"
-    //     ? document.querySelector(".userBoardContainer")
-    //     : document.querySelector(".computerBoardContainer");
     const boardContainer = document.querySelector(".computerBoardContainer");
+
+    // Remove existing "ship" class from cells
     boardContainer.querySelectorAll(".ship").forEach((ship) => {
       ship.classList.remove("ship");
     });
+
     boardArray.forEach((rowArray, rowIndex) => {
       rowArray.forEach((cellValue, columnIndex) => {
         const cell = boardContainer.querySelector(
@@ -54,6 +55,8 @@ const DOMcontroller = () => {
         if (cellValue) {
           cell.classList.add("ship"); // Add a class to indicate a ship at this cell
           cell.setAttribute("draggable", true);
+
+          // Add dragstart event listener for dragging ship cells
           cell.addEventListener("dragstart", (event) => {
             console.log("Setting drag data:", rowIndex, columnIndex, cellValue);
             event.dataTransfer.setData(
@@ -68,6 +71,8 @@ const DOMcontroller = () => {
         }
       });
     });
+
+    // Add drop event listener to handle dropping ship cells
     boardContainer.addEventListener("drop", (event) => {
       event.preventDefault();
 
@@ -107,6 +112,7 @@ const DOMcontroller = () => {
     });
   };
 
+  // Function to initialize the "Restart" button
   const initializeRestartBtn = () => {
     const restartBtn = document.querySelector(".restartBtn");
     restartBtn.addEventListener("click", () => {
@@ -116,6 +122,7 @@ const DOMcontroller = () => {
 
   let gameStarted = false;
 
+  // Function to start the game
   const startGame = () => {
     gameStarted = true;
     const startBtn = document.querySelector(".startBtn");
@@ -123,10 +130,13 @@ const DOMcontroller = () => {
     startBtn.addEventListener("click", () => {
       window.location.reload();
     });
+
+    // Disable dragging for ship cells after the game has started
     const shipcells = document.querySelectorAll(".ship");
     shipcells.forEach((shipcell) => shipcell.setAttribute("draggable", false));
   };
 
+  // Function to handle cell click during the user's turn
   const handleCellClick = (event, resolve) => {
     if (!gameStarted) {
       // Do nothing if the game hasn't started
@@ -140,10 +150,12 @@ const DOMcontroller = () => {
     }
   };
 
+  // Function to get the user's move as a Promise
   const getUserMove = () =>
     new Promise((resolve) => {
       const userBoardContainer = document.querySelector(".userBoardContainer");
 
+      // Add click event listener to handle user's move
       const handleClick = (event) => {
         handleCellClick(event, resolve);
       };
@@ -151,15 +163,18 @@ const DOMcontroller = () => {
       userBoardContainer.addEventListener("click", handleClick);
     });
 
+  // Function to initialize the "Start" button
   const initializeStartBtn = () => {
     const startBtn = document.querySelector(".startBtn");
     startBtn.addEventListener("click", startGame, { once: true });
   };
 
-  // Function to handle cell state change
+  // Function to handle cell state change (hit or missed)
   const handleCellUpdate = (coords, missedShots, player) => {
     const [row, column] = coords;
     let cell;
+
+    // Determine the target cell based on the player (user or computer)
     if (player.getName() === "user") {
       cell = document.querySelector(
         `.userBoardContainer .row:nth-child(${row + 1}) .cell:nth-child(${
@@ -173,11 +188,13 @@ const DOMcontroller = () => {
         })`,
       );
     }
+
     const isHit = !missedShots[row][column];
 
     cell.classList.add(isHit ? "hit" : "missed");
   };
 
+  // Function to display the winner in a modal
   const showWinner = (winner) => {
     const winnerModal = document.querySelector(".winnerModal");
     const winnerMessage = document.createElement("div");
@@ -191,6 +208,7 @@ const DOMcontroller = () => {
 
     winnerModal.append(winnerMessage, restartBtn);
     winnerModal.showModal();
+
     initializeRestartBtn();
   };
 
