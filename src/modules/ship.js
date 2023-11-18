08d4d91ea731fe81e680d0b12034b077c46c57ac
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-18 09:17:37 +0530

    feat: add get,set start cell in ship module

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,5 +1,7 @@
 const Ship = (length, isVertical = false) => {
   let hits = 0;
+  let startRow;
+  let startColumn;
 
   const getLength = () => length;
   const getIsVertical = () => isVertical;
@@ -8,12 +10,20 @@ const Ship = (length, isVertical = false) => {
   const hit = () => {
     if (hits < length) hits += 1;
   };
+  const getStartCell = () => [startRow, startColumn];
+  const setStartCell = (row, column) => {
+    startRow = row;
+    startColumn = column;
+  };
+
   return {
     getLength,
     getIsVertical,
     getHits,
     isSunk,
     hit,
+    getStartCell,
+    setStartCell,
   };
 };
 
